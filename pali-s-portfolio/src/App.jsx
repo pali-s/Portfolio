a12ecabe733db0d46ac7131a9960f20e9fa95726
pali-s <palishma.0812@gmail.com>
2025-05-19 17:24:29 +0545

    initial commit v3

--- pali-s-portfolio/src/App.jsx
+++ pali-s-portfolio/src/App.jsx
@@ -3,11 +3,10 @@ import questionme from './assets/images/hmmquestionmark.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-        <a href="www.linkedin.com/in/palishma-shakya-9a622529a">
+        <a href="https://www.linkedin.com/in/palishma-shakya-9a622529a">
           <img src={questionme} className="logo" alt="question me" />
         </a>
         <p>Under Construction</p>
