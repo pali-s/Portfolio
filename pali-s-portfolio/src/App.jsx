64554acee35791c13b35663f5dc4dcf9312b03e5
pali-s <palishma.0812@gmail.com>
2025-05-19 17:23:11 +0545

    initial commit v2

--- pali-s-portfolio/src/App.jsx
+++ pali-s-portfolio/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-        <a href="http://ghostwritergame.netlify.app">
+        <a href="www.linkedin.com/in/palishma-shakya-9a622529a">
           <img src={questionme} className="logo" alt="question me" />
         </a>
         <p>Under Construction</p>
